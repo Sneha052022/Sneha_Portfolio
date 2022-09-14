f4884e4fc91e2c7bf14a2330f678f948dbcd2f0f
Sneha052022 <sneha052020@gmail.com>
2022-09-14 14:59:17 +0530

    portfolio

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Sneha_Portfolio
-portfolio
+# Portfolio
+
+### This is personal Portfolio website. Visit [Portfolio]( https://snehasingh-portfolio.netlify.app)
+
+![](/src/assets/img2.png)
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/Utils/Projects.js
@@ -0,0 +1,24 @@
+export const projects = [
+  {
+    img: "https://cdn.almowafir.com/1/Banggood-846x580.jpg",
+    title: "Banggood-Clone",
+    des: "This is the clone of Banggood website. Global Leading Online Shop. Buy 3d printer, rc toys, cell phones, home appliances, tv box, home&garden, apparel with great prices on banggood.com.",
+    demo: "",
+    live: " https://banggood-clone-masai.netlify.app/",
+    gitHub: "https://github.com/Sneha052022/Banggood-Clone",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    img: "https://freight.cargo.site/w/1254/i/f65c9590835785e9abf117b729ef81313123fc43e5b4e834580b91e851f18c4d/homepage.jpg",
+    title: "Jcrew-Clone",
+    des: "J.Crew Group, Inc., is an American multi-brand, multi-channel, specialty retailer. The company offers an assortment of women's, men's, and children's apparel and accessories, including swimwear, outerwear, lounge-wear, bags, sweaters, denim, dresses, suiting, jewelry, and shoes.",
+    demo: "",
+    live: "https://jcrewclone.netlify.app/",
+    gitHub: "https://github.com/Sneha052022/timely-property-9028/tree/main/day5",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React"
+    ],
+  }];
